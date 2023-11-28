05a5a07c6e2cb8cfb41afd6cc6ebb6854799a264
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2023-11-29 06:56:52 +0700

    feat: rendering lists card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,10 @@
 const Card = (props) => {
   const { children } = props;
-  return <div className="card card-image-cover m-5">{children}</div>;
+  return (
+    <div className="card card-image-cover m-5 flex flex-col justify-between">
+      {children}
+    </div>
+  );
 };
 
 const Header = (props) => {
@@ -9,10 +13,10 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-  const { title, children } = props;
+  const { name, children } = props;
   return (
-    <div className="card-body pb-1">
-      <h2 className="card-header font-bold text-3xl">{title}</h2>
+    <div className="card-body pb-1 h-full">
+      <h2 className="card-header font-bold text-3xl">{name}</h2>
       <p className="text-content2">{children}</p>
     </div>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,43 +1,42 @@
 import Card from "../components/Card.jsx";
 import Navbar from "../components/Navbar.jsx";
 
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: "Rp 1.000.000",
+    image: "https://source.unsplash.com/random/300x200",
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam facere mollitia rem deleniti cumque.",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: "Rp 700.000",
+    image: "https://source.unsplash.com/random/300x200",
+    desc: "Lorem ipsum, adipisicing elit. Excepturi neque, reprehenderit quam facere mollitia rem deleniti cumque.",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: "Rp 500.000",
+    image: "https://source.unsplash.com/random/300x200",
+    desc: "Lorem ipsum, dolor mollitia rem deleniti cumque.",
+  },
+];
+
 const Home = () => {
   return (
     <>
       <Navbar />
       <div className="flex flex-wrap justify-center">
-        <Card>
-          <Card.Header image="https://source.unsplash.com/random/300x200" />
-          <Card.Body title="Card 1">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-            neque, reprehenderit quam facere mollitia rem deleniti cumque illum
-            eaque voluptate dignissimos impedit sit repudiandae autem nisi,
-            fugiat sapiente laboriosam dolore.
-          </Card.Body>
-          <Card.Footer>Amannn</Card.Footer>
-        </Card>
-
-        <Card>
-          <Card.Header image="https://source.unsplash.com/random/300x200" />
-          <Card.Body title="Card 2">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-            neque, reprehenderit quam facere mollitia rem deleniti cumque illum
-            eaque voluptate dignissimos impedit sit repudiandae autem nisi,
-            fugiat sapiente laboriosam dolore.
-          </Card.Body>
-          <Card.Footer>Sipsipoke</Card.Footer>
-        </Card>
-
-        <Card>
-          <Card.Header image="https://source.unsplash.com/random/300x200" />
-          <Card.Body title="Card 3">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi
-            neque, reprehenderit quam facere mollitia rem deleniti cumque illum
-            eaque voluptate dignissimos impedit sit repudiandae autem nisi,
-            fugiat sapiente laboriosam dolore.
-          </Card.Body>
-          <Card.Footer>Mantappp</Card.Footer>
-        </Card>
+        {products.map((product) => (
+          <Card key={product.id}>
+            <Card.Header image={product.image} />
+            <Card.Body name={product.name}>{product.desc}</Card.Body>
+            <Card.Footer>{product.price}</Card.Footer>
+          </Card>
+        ))}
       </div>
     </>
   );
